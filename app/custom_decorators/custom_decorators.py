90da238560f353cf4d951fb1aae6eaf7ae5db288
Josh-Ay <ayo.oloyede16@gmail.com>
2022-05-30 23:05:51 +0100

    Add user blueprint and login required decorator

--- app/custom_decorators/custom_decorators.py
+++ app/custom_decorators/custom_decorators.py
@@ -1,6 +1,5 @@
 from functools import wraps
 from flask import abort, session
-import os
 
 
 def login_required(function):
@@ -11,15 +10,4 @@ def login_required(function):
         else:
             abort(status=403)
 
-    return wrapper_function
-
-
-def admin_only(function):
-    @wraps(function)
-    def wrapper_function(*args, **kwargs):
-        if dict(session).get("ADMIN_MAIL") is not None and dict(session).get("user_profile")["email"] == os.environ.get("ADMIN_MAIL"):
-            return function(*args, **kwargs)
-        else:
-            abort(status=403)
-    
     return wrapper_function
--- app/routes_blueprint/users_blueprint.py
+++ app/routes_blueprint/users_blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from markupsafe import escape
-from app.custom_decorators.custom_decorators import login_required, admin_only
+from app.custom_decorators.custom_decorators import login_required
 
 users_blueprint = Blueprint("users_blueprint", __name__, static_folder="../../static", template_folder="../../templates")
 
@@ -9,10 +9,3 @@ users_blueprint = Blueprint("users_blueprint", __name__, static_folder="../../st
 # @login_required
 def get_user_profile(user_id):
     return render_template("user_page.html", current_user=escape(user_id))
-
-
-@users_blueprint.route("/a/all")
-# @login_required
-# @admin_only
-def show_all_users():
-    return render_template("bbbootstrap-snippet.html")
