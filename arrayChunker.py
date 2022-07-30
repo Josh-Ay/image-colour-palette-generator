fc7adae6541b353c7e016db853d0d6eba7b45ba0
Josh-Ay <ayo.oloyede16@gmail.com>
2022-07-30 13:59:49 +0100

    Add docstring to array chuncking function

--- arrayChunker.py
+++ arrayChunker.py
@@ -1,4 +1,11 @@
 def chunk_array(arr, size):
+    """ 
+        Returns array 'arr' in mini-arrays of length 'size'.
+        
+        Sample: 
+        For input values, arr = [1, 2, 3, 4, 5] and size = 2, this function would 
+        return an array with smaller arrays that each have length of 2 [[1, 2], [2, 3], [4, 5]]
+    """
     output_arrays = []
     while len(arr) > size:
         piece = arr[:size]
